84ac1d58e637f5528a9845493fe716e7f491ee15
Rillian Diello <rilliansf@gmail.com>
2023-08-08 23:59:06 -0400

    Product Controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,11 +2,90 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Productt;
+use App\Models\Unit;
+use Illuminate\Http\Request;
+
 class ProductController extends Controller
 {
-    //
-    public function index()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
     {
-        return view('app.product');
+        $products = Product::paginate(10);
+
+        return view ('app.product.index', ['products' => $products, 'request' => $request->all()]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $units = Unit::all();
+        return view('app.product.create', ['units' => $units]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Productt  $productt
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Productt $productt)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Productt  $productt
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Productt $productt)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Productt  $productt
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Productt $productt)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Productt  $productt
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Productt $productt)
+    {
+        //
     }
 }
